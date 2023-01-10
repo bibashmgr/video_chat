01c89086b68ab8b7066826084672139c67e0eb99
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2023-01-10 12:27:04 +0545

    [update]: ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { connect, io } from 'socket.io-client';
+
+import './App.css';
+
+const App = () => {
+  const BASE_URL = 'http://localhost:9999';
+
+  const [socket, setSocket] = useState(null);
+  const [chat, setChat] = useState([]);
+  const [message, setMesssage] = useState('');
+
+  useEffect(() => {
+    const newSocket = io(BASE_URL);
+    setSocket(newSocket);
+    return () => newSocket.close();
+  }, []);
+
+  socket?.on('connect', () => {
+    setChat([...chat, 'You joined the chat']);
+  });
+
+  socket?.on('newUser', (args) => {
+    setChat([...chat, args]);
+  });
+
+  socket?.on('otherMessage', (args) => {
+    setChat([...chat, args]);
+  });
+
+  const handleChange = (e) => {
+    setMesssage(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setChat([...chat, message]);
+    socket.emit('newMessage', message);
+    setMesssage('');
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='container'>
+      <div className='chat-container'>
+        {chat.map((msg, index) => {
+          return (
+            <div className='message-container' key={index}>
+              {msg}
+            </div>
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form
+        action=''
+        className='message-form'
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        <input type='text' onChange={(e) => handleChange(e)} value={message} />
+        <button type='submit'>Send</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
