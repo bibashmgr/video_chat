b4ff593a7c95a61c6ee5a2de6b4d64f5f9fb452e
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2023-01-12 17:43:36 +0545

    [cleanup]: *

--- server/app.js
+++ server/app.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const http = require('http');
 const dotenv = require('dotenv');
-// const { Server } = require('socket.io');
+const { Server } = require('socket.io');
 
 const app = express();
 dotenv.config();
 const httpServer = http.createServer(app);
-// const io = new Server(httpServer, {
-//   cors: {
-//     origin: process.env.CLIENT_BASE_URL,
-//   },
-// });
+const io = new Server(httpServer, {
+  cors: {
+    origin: process.env.CLIENT_BASE_URL,
+  },
+});
 
 // environment variables
 const PORT = process.env.PORT_NUMBER || 9999;
 
 app.get('/', (req, res) => {
   res.json('Hello');
+});
+
+io.on('connection', () => {
+  console.log('user connected');
 });
 
 httpServer.listen(PORT, () => {
