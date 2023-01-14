65387dc6131d0b4051d0a91752d0c4a77e22722a
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2023-01-14 22:37:28 +0545

    [add]: socketContext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,14 +7,19 @@ import './App.css';
 import Home from './pages/Home';
 import Room from './pages/Room';
 
+// helpers
+import { SocketContext, socket } from './helpers/Socket';
+
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/room/:roomId' element={<Room />} />
-      </Routes>
-    </Router>
+    <SocketContext.Provider value={socket}>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/room/:roomId' element={<Room />} />
+        </Routes>
+      </Router>
+    </SocketContext.Provider>
   );
 };
 
